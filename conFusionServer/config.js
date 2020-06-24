4ad30b3b30914f74980e9be6009685e5e5bf5d28
Yogesh Swami <swamiyogesh219@gmail.com>
2020-06-24 23:59:25 +0530

    OAuth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2376871532606840',
+        clientSecret: '703de160206415b8c9e832acdd535584'
+    }
 }
