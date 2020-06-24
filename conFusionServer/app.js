6e11be8b692a36f099c99c6a8dae53fc3655fed9
Yogesh Swami <swamiyogesh219@gmail.com>
2020-06-25 00:50:32 +0530

    done

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,6 +16,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 
 const mongoose = require('mongoose');
@@ -69,7 +70,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
-
+app.use('/favorites',favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
